f30d8c47c726240e8b3fa5677869a47210c2e6cc
Yauhenii <yauhenmardan@gmail.com>
2020-12-19 12:43:18 +0300

    Fix filtration bug with invert

--- algorithms/filter.py
+++ algorithms/filter.py
@@ -6,11 +6,14 @@ import numpy as np
 # FILTER
 
 def filter_grayscale_image(img, f_size):
-    # preprocess
+    # preprocessing
     img = img.copy()
     img = ImageOps.grayscale(img)
     img = np.array(img)
+    # filtration
     filtered_img = filter_image(img, f_size)
+    # postprocessing
+    filtered_img = Image.fromarray(filtered_img.astype('uint8'))
     return filtered_img
 
 
@@ -20,29 +23,32 @@ def filter_binary_image(img, f_size):
     img = img.convert('1')
     img = np.array(img)
     img = np.invert(img)
+    # filtration
     filtered_img = filter_image(img, f_size)
+    # postprocessing
+    filtered_img = Image.fromarray(filtered_img.astype('uint8'))
+    filtered_img = ImageOps.invert(filtered_img)
     return filtered_img
 
 
 def filter_image(img, f_size):
     # filter
-    w = img.shape[0]
-    h = img.shape[1]
+    width = img.shape[0]
+    height = img.shape[1]
 
-    scaled_img = np.zeros(shape=(w + 2 * f_size - 2, h + 2 * f_size - 2))
+    scaled_img = np.zeros(shape=(width + 2 * f_size - 2, height + 2 * f_size - 2))
 
-    for i in range(w):
-        for j in range(h):
+    for i in range(width):
+        for j in range(height):
             scaled_img[i + f_size - 1][j + f_size - 1] = img[i][j]
 
-    filtered_img = np.zeros(shape=(w, h))
+    filtered_img = np.zeros(shape=(width, height))
     f = f_size // 2
 
-    for i in range(f, w + f - 1):
-        for j in range(f, h + f - 1):
+    for i in range(f, width + f - 1):
+        for j in range(f, height + f - 1):
             s = []
             s = sorted(scaled_img[i - f: i + f + 1, j - f: j + f + 1].flatten(), reverse=True)
             filtered_img[i - f][j - f] = s[f_size - 1]
 
-    filtered_img = Image.fromarray(filtered_img.astype('uint8'))
     return filtered_img
